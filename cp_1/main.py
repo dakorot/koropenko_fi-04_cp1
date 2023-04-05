d12eb347c9ee51b286ffb52bbf0d9d046a684228
Daria K <koropenko.daria@gmail.com>
2023-04-06 00:25:08 +0300

    1) A function for calculating entropy for each letter in the given file H_1 = P(x_1 = z_1, ...) + a small test has been added
    2) A function for calculating entropy for each bigram in the given file H_2 = P(x_1 = z_1, ...) + a small test has been added

--- cp_1/main.py
+++ cp_1/main.py
@@ -31,6 +31,10 @@ def count_letters():
             if letter.isalpha():
                 print(f'{letter}: {letter_count[letter]}')
 
+        print('Num. of all the letters:', sum(letter_count.values()))
+
+        return letter_count
+
 def count_bigrams():
     input_file = 'output.txt'
 
@@ -39,7 +43,33 @@ def count_bigrams():
 
         bigrams_count = collections.Counter(content[i : i + 2] for i in range(len(content) - 1))
 
-        for letter in sorted(bigrams_count):
-            if letter.isalpha():
-                print(f'{letter}: {bigrams_count[letter]}')
+        for bigram in sorted(bigrams_count):
+            if bigram.isalpha():
+                print(f'{bigram}: {bigrams_count[bigram]}')
 
+        print('Num of all the bigrams is', sum(bigrams_count.values()))
+
+        return bigrams_count
+
+def entropy_1(letter_freq, total_letter_num):
+    entropy = letter_freq / total_letter_num
+
+    return entropy
+
+letter = count_letters() #it's a dictionary
+Z = letter.keys()
+
+for z_i in sorted(Z):
+    letter_frequency = entropy_1(letter[z_i], sum(letter.values()))
+    print(f'For {z_i} the P = {letter_frequency:.3f}')
+def entropy_2(bigram_freq, total_bigram_num):
+    entropy = bigram_freq / total_bigram_num
+
+    return entropy
+
+bigram = count_bigrams() #it's a dictionary
+Z = bigram.keys()
+
+for z_i in sorted(Z):
+    bigram_frequency = entropy_1(bigram[z_i], sum(bigram.values()))
+    print(f'For {z_i} the P = {bigram_frequency:.3f}')
